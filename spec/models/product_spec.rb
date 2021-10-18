7acc8fd10c8bf0daca6b7da3a74ec46b00dd312d
Rupi-Jain <rupi.jain@gmail.com>
2021-10-18 18:55:10 -0400

    tests for product are added

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  
+  describe 'Validations' do
+    
+    describe 'validation for names' do
+
+      it "product has everything" do
+        @category = Category.new(name: "name")
+        @product = Product.new(
+          name: 'name',
+          price_cents: 2000,
+          quantity: 2,
+          category: @category
+        ) 
+        expect(@product).to be_valid
+      end
+   
+
+      it "has no name" do
+        @category = Category.new(name: "name")
+        @product = Product.new(
+          name: '',
+          price_cents: 2000,
+          quantity: 2,
+          category: @category
+        ) 
+        expect(@product).to_not be_valid
+        expect(@product.errors[:name].size).to eq(1)
+      end
+    end    
+
+    it "has no price" do
+      @category = Category.new(name: "name")
+      @product = Product.new(
+        name: 'name',
+        price_cents: nil ,
+        quantity: 2,
+        category: @category
+      ) 
+      expect(@product).to_not be_valid
+      expect(@product.errors[:price_cents].size).to eq(1)
+    end
+
+    it "has no quantity" do
+      @category = Category.new(name: "name")
+      @product = Product.new(
+        name: 'name',
+        price_cents: 2000 ,
+        quantity: nil,
+        category: @category
+      ) 
+      expect(@product).to_not be_valid
+      expect(@product.errors[:quantity].size).to eq(1)
+    end
+
+    it "has no category" do
+      @product = Product.new(
+        name: 'name',
+        price_cents: 2000 ,
+        quantity: 2,
+      ) 
+      expect(@product).to_not be_valid
+      expect(@product.errors[:category].size).to eq(1)
+    end
+
+  end
+end
