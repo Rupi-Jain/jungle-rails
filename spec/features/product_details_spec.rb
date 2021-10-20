1c80dbd3f751b0b8c61f4a87cdebd79ed0db582d
Rupi-Jain <rupi.jain@gmail.com>
2021-10-19 22:59:06 -0400

    final commit to product details

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature, js: true do
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
 
@@ -12,19 +14,22 @@ RSpec.feature "ProductDetails", type: :feature, js: true do
         quantity: 10,
         price: 64.99
       )
+
     end
   end
 
-  scenario "They see the selected product" do
+  scenario "They see the selected product detail page" do
     # ACT
     visit root_path
-
+  
     all('article.product img').first.click
-    
+
+    #DEBUG
     sleep 5
     save_screenshot
-
-   expect(page).to have_css 'section.product-show', count: 1
-
+    
+    # VERIFY
+    expect(page).to have_css 'section.products-show', count: 1
+   
   end
 end
